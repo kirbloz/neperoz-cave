4b08e6a1db41abfa0344120035fbe640679038e3
kirbloz <one.baisini@gmail.com>
2026-07-19 05:27:05 +0200

    idk what this is

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://neperoz-cave.nekoweb.org",
+  integrations: [mdx(), sitemap()] /*,
 	fonts: [
 		{
 			provider: fontProviders.local(),
@@ -31,5 +31,5 @@ export default defineConfig({
 				],
 			},
 		},
-	],
+	]*/,
 });
